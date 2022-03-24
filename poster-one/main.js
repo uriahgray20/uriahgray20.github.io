db9d8aedf4295cceb14d78d449200183a7708c02
Uriah Gray <u@u-p.co>
2022-03-25 08:15:47 +1100

    upload posters

---
+++ poster-one/main.js
@@ -0,0 +1,34 @@
+$(function(){
+    console.log("I'm ready now");
+    
+    var num = ["one", "two", "three"];
+    var abc = ["a", "b", "c"];
+    var xyz = ["x", "y", "z"];
+
+    function randomClass() {
+        const synth = new Tone.MonoSynth().toMaster();
+        synth.triggerAttackRelease("G2", "6n");
+        $(".text").each(function () {
+            $(this).removeClass(num);
+            $(this).addClass(num[Math.floor(Math.random() * num.length)]);
+        });
+        $(".text").each(function () {
+            $(this).removeClass(abc);
+            $(this).addClass(abc[Math.floor(Math.random() * abc.length)]);
+        });
+        $(".text").each(function () {
+            $(this).removeClass(xyz);
+            $(this).addClass(xyz[Math.floor(Math.random() * xyz.length)]);
+        });                
+        setTimeout(randomClass, 1500)
+    }
+
+    
+
+    $(".play-button").click(function () {
+        Tone.Transport.start();
+        randomClass();
+        $(".play-wrapper").hide();
+      });
+
+});
