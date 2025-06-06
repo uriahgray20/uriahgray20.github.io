6fd6bbfc758f7680ba4d2db4885669b851077a7d
Uriah Gray <u@u-p.co>
2025-06-06 13:07:24 +1000

    upload website

--- class-changes.js
+++ class-changes.js
@@ -1,23 +1,15 @@
 const classChanges = [
-  { time: 4, target: ".pop3", add: "active" },
-  { time: 8, target: ".pop3", remove: "active" },
 
-  { time: 5, target: ".pop2", add: "active" },
-  { time: 9, target: ".pop2", remove: "active" },
+  { time: 4, target: ".p1", add: "highlight" },
 
-  { time: 18, target: ".mainpage", add: "active" },
+  { time: 6, target: ".p1", remove: "highlight" },
+  { time: 6, target: ".p2", add: "highlight" },
 
-  { time: 8, target: ".p1", add: "active" },
+  { time: 8, target: ".p2", remove: "highlight" },
+  { time: 8, target: ".p3", add: "highlight" },  
 
-  { time: 10, target: ".p2", add: "active" },
 
-  { time: 13, target: ".p3", add: "active" },
+  { time: 10, target: ".p3", remove: "highlight" },
+  { time: 10, target: ".p4", add: "highlight" },  
 
-  { time: 20, target: ".mainpage", remove: "active" },
-  { time: 16, target: ".changeme2", remove: "show" },
-  { time: 16, target: ".changeme2", add: "hidden" },
-
-  { time: 16, target: ".changeme3", add: "show" },
-  { time: 28, target: ".changeme3", remove: "show" },
-  { time: 28, target: ".changeme3", add: "hidden" }
 ];
